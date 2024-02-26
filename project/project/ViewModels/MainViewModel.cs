a746f1ac801f9d7cc178d9e3b28b33bc4ff22f74
EgorZak <egorzakruzhnui@gmail.com>
2024-02-26 22:27:49 +0300

    design version mainView

---
+++ project/project/ViewModels/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using FontAwesome.Sharp;
+using project.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace project.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        //Fields
+   
+        private ViewModelBase _currentChildView;
+   
+        //Properties
+   
+        public ViewModelBase CurrentChildView
+        {
+            get
+            {
+                return _currentChildView;
+            }
+            set
+            {
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            }
+        }
+   
+    
+        public ICommand ShowHomeViewCommand { get; }
+        public ICommand ShowCustomerViewCommand { get; }
+        public MainViewModel()
+        {
+          
+            //Initialize commands
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+            ShowCustomerViewCommand = new ViewModelCommand(ExecuteShowCustomerViewCommand);
+            //Default view
+            ExecuteShowHomeViewCommand(null);
+        
+        }
+        private void ExecuteShowCustomerViewCommand(object obj)
+        {
+            CurrentChildView = new CustomerViewModel();
+       
+        }
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            CurrentChildView = new HomeViewModel();
+      
+        }
+    }
+}
